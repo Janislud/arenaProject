746f46e35a2d2c9371623e32a31c3b0fa23e7c6d
Janislud <janisludborzs2@gmail.com>
2023-09-20 23:01:51 +0200

    "fix width"

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ secondSectionDivWrapper.classList = "second-section-wrapper-block"
 for( let i = 0; i < 3;i++) {
     const div = document.createElement('div')
     const img = document.createElement('img')
+    const br = document.createElement('br')
     const rectangel = document.createElement('img')
     const h6 = document.createElement('h6')
     const p = document.createElement('p')
@@ -59,6 +60,7 @@ for( let i = 0; i < 3;i++) {
     p.innerHTML='Мы сделали самую красивую арену в Европе. Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. Мы очень рады данному еву.'
 
     div.append(img)
+    div.append(br)
     div.append(rectangel)
     div.append(h6)
     div.append(p)
@@ -76,14 +78,20 @@ const threeSectionDiv = document.createElement('div')
 threeSectionDiv.classList = 'three-section-text-wrapper'
 threeSection.append(threeSectionDiv)
 const threeSectionH2 = document.createElement('h2')
-threeSectionDiv.append(threeSectionH2)
+const thrreeSectionDivSecond = document.createElement('div')
+thrreeSectionDivSecond.classList='three-text-wrapper'
+// threeSectionDiv.append(threeSectionH2)
 threeSectionH2.innerText= 'САМЫЕ УМНЫЕ ПРОЕКТЫ'
 const threeSectionP = document.createElement('p')
 threeSectionP.innerText = 'РЕАЛИЗУЕМ САМЫЕ СМЕЛЫЕ РЕШЕНИЯ В РОССИИ!'
-threeSectionDiv.append(threeSectionP)
+// threeSectionDiv.append(threeSectionP)
+threeSectionDiv.append(thrreeSectionDivSecond)
+thrreeSectionDivSecond.append(threeSectionH2)
+thrreeSectionDivSecond.append(threeSectionP)
+
 const threeSectionBtn = document.createElement('button')
 threeSectionBtn.innerText='ВАШ ЗАПРОС'
-threeSection.append(threeSectionBtn)
+threeSectionDiv.append(threeSectionBtn)
 
 // Four section
 
